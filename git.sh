288129bc86cfe952483db5331879af50f4c5f1c9
Yuhi-Sato <yuhi120101@gmail.com>
2024-11-30 05:48:12 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+git init
+ssh -T git@github.com
+git remote add origin git@github.com:Yuhi-Sato/isucon12-f.git
+git add .
+git commit -m 'first commit'
+git branch -M main
+git push origin main
---
+++ setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/Yuhi-Sato/isucon-ready/main/Makefile
+make setup
+make set-as-s1
+make get-conf
+cat ~/.ssh/id_ed25519.pub
